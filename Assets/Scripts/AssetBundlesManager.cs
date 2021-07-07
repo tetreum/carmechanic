c7767447c4f5c4d6127c071bf36167b934f5c8e9
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-07-07 08:31:33 +0100

    commit

--- Assets/Scripts/AssetBundlesManager.cs
+++ Assets/Scripts/AssetBundlesManager.cs
@@ -14,7 +14,7 @@ public class AssetBundlesManager : MonoBehaviour
         {
             var carsbundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "cars"));
             var car1 = carsbundle.LoadAsset<GameObject>("car1");
-            Instantiate(car1,new Vector3(-24,0,-2),new Quaternion(0,45,0,0));
+            Instantiate(car1,new Vector3(-24,0,-2),Quaternion.Euler(0,90,0));
         }
     }
 }
