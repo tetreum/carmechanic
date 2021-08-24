8ae1c5d3377c532d11826af8489bdccd4375a53a
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-08-24 22:59:22 +0100

    commit

--- Assets/Scripts/Editor/CreateAssetBundles.cs
+++ Assets/Scripts/Editor/CreateAssetBundles.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class CreateAssetBundles : MonoBehaviour
 {
-    [MenuItem("AssetBundles/generate windows")]
+    [MenuItem("AssetBundles/build/windows")]
     private static void BuildAllAssetBundlesWindows()
+    {
+        var assetBundleDirectory = Application.streamingAssetsPath;
+        if (!Directory.Exists(assetBundleDirectory)) Directory.CreateDirectory(assetBundleDirectory);
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory,
+            BuildAssetBundleOptions.None,
+            BuildTarget.StandaloneWindows);
+    }
+
+    [MenuItem("AssetBundles/build/windows64")]
+    private static void BuildAllAssetBundlesWindows64()
     {
         var assetBundleDirectory = Application.streamingAssetsPath;
         if (!Directory.Exists(assetBundleDirectory)) Directory.CreateDirectory(assetBundleDirectory);
