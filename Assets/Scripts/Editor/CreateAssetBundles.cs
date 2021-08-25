82e6f7f3293491499027c6f3ed6f59e615ca91f1
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-08-25 15:56:36 +0100

    commit
    Signed-off-by: Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>

--- Assets/Scripts/Editor/CreateAssetBundles.cs
+++ Assets/Scripts/Editor/CreateAssetBundles.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CreateAssetBundles : MonoBehaviour
 {
-    [MenuItem("AssetBundles/build/assetbundles/windows")]
+    [MenuItem("build/assetbundles/windows")]
     private static void BuildAllAssetBundlesWindows()
     {
         var assetBundleDirectory = Application.streamingAssetsPath;
@@ -14,7 +14,7 @@ public class CreateAssetBundles : MonoBehaviour
             BuildTarget.StandaloneWindows);
     }
 
-    [MenuItem("AssetBundles/build/assetbundles/windows64")]
+    [MenuItem("build/assetbundles/windows64")]
     private static void BuildAllAssetBundlesWindows64()
     {
         var assetBundleDirectory = Application.streamingAssetsPath;
