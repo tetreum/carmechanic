63e49b3e16d2b12e402f8b5270c73171744026e3
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-08-30 17:49:54 +0100

    commit

--- Assets/Scripts/Manager/AudioManager.cs
+++ Assets/Scripts/Manager/AudioManager.cs
@@ -1,3 +1,4 @@
+using System;
 using FMOD.Studio;
 using FMODUnity;
 using UnityEngine;
@@ -9,6 +10,22 @@ public class AudioManager : MonoBehaviour
     private void Awake()
     {
         footsteps = RuntimeManager.CreateInstance("event:/footsteps");
+    }
+
+    private void Update()
+    {
+        if (Player.canMove)
+        {
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.A) ||
+                Input.GetKeyDown(KeyCode.D))
+            {
+                footsteps.start();
+                RuntimeManager.AttachInstanceToGameObject(footsteps, transform);
+            }
+
+            if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.A) ||
+                Input.GetKeyUp(KeyCode.D)) footsteps.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        }
     }
 
     private void OnDestroy()
